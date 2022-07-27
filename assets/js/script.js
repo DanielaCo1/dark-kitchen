52c6a46bc3d05ac44e45a10da469a497a0cd6826
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2022-07-27 11:49:17 +0200

    create first table

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const Food = [
+    {
+        name: "Maki saumon",
+        price: "600",
+        piece: "6 pièces",
+        compostion: ["Saumon", "Algues", "Riz"],
+        picture: "assets\images\maki-saumon.png",
+        description: "Le maki Saumon est à l'image du concept, simple, frais et de qualité. Succombez à cette bouchée de fraicheur.",
+    },
+    {
+        name: "Maki thon",
+        price: "400",
+        piece: "6 pièces",
+        compostion: ["Thon", "Algues", "Riz"],
+        picture: "assets\images\maki-thon.png",
+        description: "Le maki Thon est à l'image du concept, simple, frais et de qualité. Succombez à cette bouchée de fraicheur.",
+    },
+    {
+        name: "Maki avocat",
+        price: "400",
+        piece: "6 pièces",
+        compostion: ["Avocat", "Algues", "Riz"],
+        picture: "assets\images\maki-avocat.png",
+        description: "Le maki Avocat est le choix parfait pour un plaisir végétarien, laissez vous séduire par l'onctuosité de l'avocat.",
+    },
+];
